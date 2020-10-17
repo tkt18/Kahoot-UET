834deb8f5c278fdc682837040a45a4fd020c03d0
hopdt1404 <hopdt1404@gmail.com>
2020-10-17 14:11:31 +0700

    create table topics

---
+++ kahoot-uet/database/migrations/2020_10_17_065334_create_topics_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTopicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('topics', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable(false);
+            $table->unsignedBigInteger('creator_id')->nullable(false);
+            $table->boolean('is_deleted');
+            $table->boolean('is_public');
+            $table->boolean('is_daft');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('topics');
+    }
+}
