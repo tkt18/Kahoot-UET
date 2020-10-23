5d07fad0362307af596f0144da5ea5961386c19c
hopdt1404 <hopdt1404@gmail.com>
2020-10-23 12:31:28 +0700

    create fake data Room

--- kahoot-uet/database/factories/RoomFactory.php
+++ kahoot-uet/database/factories/RoomFactory.php
@@ -6,7 +6,12 @@ use App\Rooms;
 use Faker\Generator as Faker;
 
 $factory->define(Rooms::class, function (Faker $faker) {
+    $minValPin = 10000;
+    $maxValPin = 9999999999;
     return [
-        //
+        'PIN' => rand($minValPin, $maxValPin),
+        'creator_id' => rand(1, 100),
+        'is_lock' => rand(0, 1),
+        'is_finish' => rand(0, 1)
     ];
 });
