681124f3b27dccdc97465ea154c42534081149cc
hopdt1404 <hopdt1404@gmail.com>
2020-10-24 15:27:28 +0700

    update table room

---
+++ kahoot-uet/database/migrations/2020_10_24_082426_add_column_topic_id_to_tabel_room.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnTopicIdToTabelRoom extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('rooms', function (Blueprint $table) {
+            $table->unsignedBigInteger('topic_id')->nullable(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('rooms', function (Blueprint $table) {
+            //
+        });
+    }
+}
