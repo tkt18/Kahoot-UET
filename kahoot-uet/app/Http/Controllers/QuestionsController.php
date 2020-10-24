e21c3da9e74663438fbb1d9825bea50306d8a29c
hopdt1404 <hopdt1404@gmail.com>
2020-10-24 18:11:20 +0700

    done step1 question method getQuestionByTopicId

--- kahoot-uet/app/Http/Controllers/QuestionsController.php
+++ kahoot-uet/app/Http/Controllers/QuestionsController.php
@@ -8,7 +8,8 @@ use Illuminate\Http\Request;
 class QuestionsController extends Controller
 {
     public function index () {
-        $myQuestion = Questions::where('id', '>', 0)->get();
+        $topicId = 3;
+        $myQuestion = Questions::select('sequence', 'title', 'time', 'question_type', 'question_type_select', 'score', 'answer', 'question_img')->where('topic_id', $topicId)->get();
         return view('pages.topic', ['data' => $myQuestion]);
     }
 }
