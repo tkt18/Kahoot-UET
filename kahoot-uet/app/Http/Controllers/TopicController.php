b297434bc1f6513c3015c5b142edbc8a24519e84
hopdt1404 <hopdt1404@gmail.com>
2020-10-24 18:10:31 +0700

    done step1 get topic info

--- kahoot-uet/app/Http/Controllers/TopicController.php
+++ kahoot-uet/app/Http/Controllers/TopicController.php
@@ -15,7 +15,7 @@ class TopicController extends Controller
     public function index () {
         $creator_id = 1;
 
-        $myTopic = Topics::select('id' ,'name', 'creator_id', 'created_at', 'is_public', 'is_daft', 'is_played')->where('creator_id', $creator_id)->where('is_deleted', 0)->get();
+        $myTopic = Topics::select('id' ,'name', 'creator_id', 'created_at', 'is_public', 'is_daft', 'is_played', 'created_at')->where('creator_id', $creator_id)->where('is_deleted', 0)->get();
         $result = [];
         for ($i = 0; $i < count($myTopic); $i++) {
             $topic = $myTopic[$i];
